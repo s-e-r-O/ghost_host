8228c793e88b1f0b7d5009c6c752756a8b374741
Sergio Oporto Tejerina <ser.op.te@gmail.com>
2017-10-05 13:34:31 -0400

    Added a reader for ethernet datagram

--- include/recv.h
+++ include/recv.h
@@ -1,3 +1,6 @@
 #include <pcap/pcap.h>
 
 void pcap_callback(u_char * user, const struct  pcap_pkthdr *h, const u_char *bytes);
+void ether_reader(const u_char *bytes, bpf_u_int32 data_len);
+void ip_reader(const u_char *bytes, bpf_u_int32 data_len);
+void arp_reader(const u_char *bytes, bpf_u_int32 data_len);
---
+++ source/ether_reader.c
@@ -0,0 +1,22 @@
+#include <net/ethernet.h>
+#include <netinet/ether.h>
+
+#include "recv.h"
+
+/* IEEE Standard for Ethernet -> http://standards.ieee.org/getieee802/download/802.3-2012_section1.pdf */
+
+void ether_reader(const u_char *bytes, bpf_u_int32 data_len)
+{
+	struct ether_header *headerEthernet = (struct ether_header *) bytes;
+	  
+	switch(ntohs(headerEthernet->ether_type)){
+		case ETHERTYPE_IP:
+			// Trying to find an ICMP package
+			ip_reader(bytes + sizeof(*headerEthernet), data_len - sizeof(*headerEthernet));
+			break;
+		case ETHERTYPE_ARP:
+			// Trying to reach a MAC Address
+			arp_reader(bytes + sizeof(*headerEthernet), data_len - sizeof(*headerEthernet));
+			break;
+	  }
+}
