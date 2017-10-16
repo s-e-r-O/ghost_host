5c4ecc8a00d9641bbdc5caae210be4d4ba905ffc
Sergio Oporto Tejerina <ser.op.te@gmail.com>
2017-10-16 01:20:15 +0000

    Implemented a way to use libnet and libpcap data across all files

--- include/conf-data.h
+++ include/conf-data.h
@@ -1,13 +1,16 @@
-#include <pcap/pcap.h>
-#include <libnet.h>
+#ifndef CONF_H_
+#define CONF_H_
+	#include <pcap/pcap.h>
+	#include <libnet.h>
 
-struct configuration
-{
-	struct 
+	struct configuration
 	{
-	    u_int32_t ip_addr; // IP address for the ghost host. It has to be in the same net as requesting hosts.
-	    u_int8_t* hrd_addr; // MAC address for the ghost host.
-	} ghost_host;
-	pcap_t *p;
-	libnet_t *l;	
-};
+		struct 
+		{
+		    u_int32_t ip_addr; // IP address for the ghost host. It has to be in the same net as requesting hosts.
+		    u_int8_t* hrd_addr; // MAC address for the ghost host.
+		} ghost_host;
+		pcap_t *p;
+		libnet_t *l;	
+	};
+#endif
